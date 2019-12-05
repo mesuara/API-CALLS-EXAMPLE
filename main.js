85742ce78b70b03141bbedc5523fb9e6f875361f
mesuara <mesuara@gmail.com>
2019-12-05 12:27:01 -0500

    added the template for the cards

---
+++ main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const books = [
+  { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+  { title: 'A Farewell to Arms', author: 'Ernest Hemingway' },
+  { title: 'Catch 22', author: 'Joseph Heller' }
+];
+
+function appendBooks(templateId) {
+    const booksList = document.getElementById('books');
+    const fragment = document.getElementById(templateId);
+    
+    // Clear out the content from the ul
+    booksList.innerHTML = '';
+    
+    // Loop over the books and modify the given template
+    books.forEach(book => {
+      // Create an instance of the template content
+      const instance = document.importNode(fragment.content, true);
+      // Add relevant content to the template
+      instance.querySelector('.title').innerHTML = book.title;
+      instance.querySelector('.author').innerHTML = book.author;
+      // Append the instance ot the DOM
+      booksList.appendChild(instance);
+    });  
+  }
+
+  appendBooks('book-template')
